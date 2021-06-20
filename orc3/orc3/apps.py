14c9b01729781b7547fc8970abb41c1101e9e8a2
Ruan Jansen van Vuuren <ruan@inventixx.com>
2021-06-20 17:27:25 +0200

    oof

--- orc3/orc3/apps.py
+++ orc3/orc3/apps.py
@@ -1,16 +1,12 @@
 from django.apps import AppConfig
 import pickle
-import html
-import pathlib
 import os
 
 
 class PredictorConfig(AppConfig):
-    # create path to models
+
     path = os.path.join('../models/TrainedModel.p')
 
-    # load models into separate variables
-    # these will be accessible via this class
     with open(path, 'rb') as pickled:
         data = pickle.load(pickled)
     print(data)
